669f959ae8c26a0d92e161d4e6522fd6dfd06b8b
zhhrozhh <zhangh40@msu.edu>
2019-06-17 16:25:16 +0100

    z

--- tools.py
+++ tools.py
@@ -1,11 +1,12 @@
 import iexfinance
+from iexfinance.stocks import get_historical_data
 import pandas as pd
 
 def get_data(scode,start = None,end = None,last = None):
     scode = scode.replace('_','.')
     if last:
         return get_last(scode,last)
-    data = pd.DataFrame(iexfinance.stocks.get_historical_data(scode,start = start,end = end)).T
+    data = pd.DataFrame(get_historical_data(scode,start = start,end = end,token = "pk_4d900afee23b484ba28610b49b795dcf")).T
     if not len(data):
         data = pd.DataFrame(columns = ['Adj_Close','Adj_High','Adj_Low','Adj_Open','Adj_Volume'])
     data.columns = ['Adj_Close','Adj_High','Adj_Low','Adj_Open','Adj_Volume']
